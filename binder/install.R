608131af1959a86079492d13f231ec725d09ace1
Pakillo <f.rodriguez.sanc@gmail.com>
2022-01-25 20:29:35 +0100

    now?

--- binder/install.R
+++ binder/install.R
@@ -1,3 +1,3 @@
 ### install GitHub packages (tag = commit, branch or release tag)
 install.packages("remotes")
-remotes::install_github("brianstock/MixSIARgui", ref = "2a14a2eb47096246dce7bbc047d4dd8cd7dc419d")
+remotes::install_github("brianstock/MixSIARgui")
