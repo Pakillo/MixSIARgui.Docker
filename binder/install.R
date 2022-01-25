ccecf163191f1f8022d0a2544a904a2a1500f3d9
Pakillo <f.rodriguez.sanc@gmail.com>
2022-01-25 20:36:31 +0100

    closely following readme

--- binder/install.R
+++ binder/install.R
@@ -1,3 +1,5 @@
 ### install GitHub packages (tag = commit, branch or release tag)
+install.packages("RGtk2")
+install.packages("MixSIAR")
 install.packages("remotes")
 remotes::install_github("brianstock/MixSIARgui")
