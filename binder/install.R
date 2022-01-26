144cdf4ae5ddcdf49367aba7d57baef46452bbd3
Pakillo <f.rodriguez.sanc@gmail.com>
2022-01-26 11:20:49 +0100

    keep trying

--- binder/install.R
+++ binder/install.R
@@ -1,5 +1,5 @@
 ### install GitHub packages (tag = commit, branch or release tag)
-install.packages("rjags")
+# install.packages("rjags")
 install.packages("RGtk2")
 install.packages("MixSIAR")
 install.packages("remotes")
